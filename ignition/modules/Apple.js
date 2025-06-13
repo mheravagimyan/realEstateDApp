5dc395bd90c30f2b320b91b735a26f35707a24bb
reshaly <mheravagimian@gmail.com>
2025-06-13 21:16:21 +0300

    add new packages

--- ignition/modules/Apple.js
+++
@@ -1,5 +0,0 @@
-// const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-// module.exports = buildModule("AppleModule", (m) => {
-//   const appleContract = m.contract("Apple", [] );
-//   return { appleContract };
-// });
